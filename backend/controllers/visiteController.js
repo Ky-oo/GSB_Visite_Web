03b3a4e70ec4a096e526b713e7c7907b79ad1c82
Kylian <113977736+Ky-oo@users.noreply.github.com>
2024-04-10 10:00:31 +0000

    update

--- backend/controllers/visiteController.js
+++ backend/controllers/visiteController.js
@@ -1,33 +1,39 @@
 const Visite = require('../models/visite');
+const Praticien  = require('../models/praticien');
+const Visiteur = require('../models/visiteur');
+const Motif = require('../models/motif');
 const expressAsyncHandler = require('express-async-handler');
+const e = require('express');
 
 exports.createVisite = expressAsyncHandler(async (req, res) => {
+    console.log(req.body);
+
     const visite = new Visite({
-        date_visite: req.body.date_visite,
+        date_visite: new Date(req.body.date_visite),
         commentaire: req.body.commentaire,
         visiteur: req.body.visiteur,
         praticien: req.body.praticien,
         motif: req.body.motif
     });
-
+    console.log(visite);
     try {
         await visite.save();
-        await praticien.findByAndUpdated({ _id: req.body.praticien }, (err, praticien) => {
-            $push: { visites: visite._id }}, {new: true, useFindAndModify: false
-        });
-
-        await visiteur.findByAndUpdated({ _id: req.body.visiteur }, (err, visiteur) => {
-            $push: { visites: visite._id }}, {new: true, useFindAndModify: false
-        });
-
+        await Praticien.findOneAndUpdate({ _id: req.body.praticien }, {
+            $push: { visites: visite._id }
+        }, {new: true, useFindAndModify: false});
+        console.log('praticien updated');
+        await Visiteur.findOneAndUpdate({ _id: req.body.visiteur }, {
+            $push: { visites: visite._id }
+        }, {new: true, useFindAndModify: false});
+        console.log('visiteur updated');
         res.status(201).json({ message: 'Visite created successfully!'});
     }
     catch (error) {
+        console.log(error);
         res.status(400).json({ error });
         return;
     }
-}
-);
+});
 
 exports.getOneVisite = expressAsyncHandler(async (req, res) => {
     const visite = await Visite.findOne({ _id: req.params.id }).populate('visiteur').populate('praticien').populate('motif');
